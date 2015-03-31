96d3068cd87b0eed8bef1db977251ee14e661d52
Andy Chen <chenyulong1@gmail.com>
2015-03-31 17:23:59 +0800

    init project

---
+++ ios_modular_dev_demo/ios_modular_dev_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_modular_dev_demo
+//
+//  Created by Andy on 3/31/15.
+//  Copyright (c) 2015 eleme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
